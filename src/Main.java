d2a3de50bdaf9833a2410c6d46a66522e98f8772
Daniil Kirichenco <mannol.2015@mail.ru>
2024-04-11 17:36:31 +0300

    chenge-3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        boolean passIsValid = false;
-        boolean inCarantine = false;
+        boolean passIsValid = true;
+        boolean inCarantine = true;
 
         String message = passIsValid && !inCarantine?
                 "Проход открыт":"Проход закрыт";
